8bef4817e4b75b2f9f501fab706598e4aa3b1af7
Luis Joel Hernandez <l.hernandez@gbh.com.do>
2021-02-20 09:39:30 -0400

    width & height fields added

--- ejercicio_2/ejercicio_2.js
+++ ejercicio_2/ejercicio_2.js
@@ -1,10 +1,13 @@
 const form = document.getElementById('form');
+const div = document.getElementById('div');
 
 const submit = function(event) {
   event.preventDefault();
 
-  document.body.style.backgroundColor = event.target.backgroundColor.value;
-  document.body.style.fontSize = event.target.fontSize.value;
+  div.style.backgroundColor = event.target.backgroundColor.value;
+  div.style.fontSize = `${event.target.fontSize.value}px`;
+  div.style.width = Math.min(event.target.width.value, 200);
+  div.style.height = Math.min(event.target.height.value, 200);
 }
 
 form.addEventListener('submit', submit);
